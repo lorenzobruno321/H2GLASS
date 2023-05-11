c1fd7ad8be16d959f31ef914eea99ba247f71b02
lorenzobruno321 <131887938+lorenzobruno321@users.noreply.github.com>
2023-05-11 11:17:52 +0200

    Add files via upload

--- plotting_figures.m
+++ plotting_figures.m
@@ -21,36 +21,5 @@ ylabel('thermal load [MW]')
 title('Approximation of thermal load in a steel furnace in one year')
 set(gcf,'color','w')
 
-[AA list_pv list_grid]=xlsread('Results.xlsx');
 
-figure(2)
-plot(AA(:,1),AA(:,2),'r-')               
-hold on
-plot(AA(:,1),AA(:,3),'b-')
-box on
-grid on
-xlabel('time [h]')
-% ylabel('thermal load [MW]')
-legend('PV power [kW]','GRID power [kW]')
-title('Power supply to the electrolyser to produce hydrogen')
-set(gcf,'color','w')
-
-figure(3)
-plot(AA(:,1),AA(:,2),'r-')               
-hold on
-box on
-grid on
-xlabel('time [h]')
-ylabel('PV power supply [kW]')
-title('Power supply from the PV generation')
-set(gcf,'color','w')
-
-figure(4)
-plot(AA(:,1),AA(:,3),'b-')               
-hold on
-box on
-grid on
-xlabel('time [h]')
-ylabel('GRID power supply [kW]')
-title('Power supply from the GRID')
-set(gcf,'color','w')
+[time_vec list_pv list_grid]=xlsread('Results.xls')
